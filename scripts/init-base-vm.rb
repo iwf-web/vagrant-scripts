50876e596b16d82524a9de30449598dcd246c331
Jens Hassler <j.hassler@iwf.ch>
2019-11-15 13:33:36 +0100

    temporary fix for kernel-headers issue

--- scripts/init-base-vm.rb
+++ scripts/init-base-vm.rb
@@ -4,6 +4,7 @@
 def init_base_vm(config)
     config.vm.box = "debian/contrib-stretch64"
     config.vm.box_check_update = false
+    config.vbguest.auto_update = false # temporary until base box is updated (see https://github.com/dotless-de/vagrant-vbguest/issues/351)
 
     config.ssh.username = "vagrant"
 end
