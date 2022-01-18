a4bdb0cec4d791f4f9c2361046b51cb1543017c7
D3strukt0r <dev@d3strukt0r.me>
2022-01-18 09:14:30 +0100

    Update from Debian 9 to 11

--- scripts/init-base-vm.rb
+++ scripts/init-base-vm.rb
@@ -1,27 +1,9 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-# see this issue: https://github.com/dotless-de/vagrant-vbguest/issues/351
-class WorkaroundVbguest < VagrantVbguest::Installers::Linux
-    def install(opts=nil, &block)
-      puts 'Executing update workaround (system and kernel upgrade)...'
-      communicate.sudo('apt-get -y --force-yes update', (opts || {}).merge(:error_check => false), &block)
-      communicate.sudo('apt-get -y --force-yes upgrade', (opts || {}).merge(:error_check => false), &block)
-      communicate.sudo('apt-get -y install build-essential linux-headers-amd64 linux-image-amd64', (opts || {}).merge(:error_check => false), &block)
-      puts 'Continue vbguest installation...'
-      super
-      puts 'Perform vbguest post installation steps...'
-      communicate.sudo('usermod -a -G vboxsf vagrant', (opts || {}).merge(:error_check => false), &block)
-    end
-    def reboot_after_install?(opts=nil, &block)
-      true
-    end
-end
-
 def init_base_vm(config)
-    config.vm.box = "debian/contrib-stretch64"
+    config.vm.box = "bento/debian-11"
     config.vm.box_check_update = false
-    config.vbguest.installer = WorkaroundVbguest
 
     config.ssh.username = "vagrant"
 end
--- scripts/prov-install-docker.rb
+++ scripts/prov-install-docker.rb
@@ -4,17 +4,16 @@
 def prov_install_docker(config)
     $script = <<-SCRIPT
         sudo apt-get update
-        sudo apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common curl
-        curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-        sudo add-apt-repository \
-           "deb [arch=amd64] https://download.docker.com/linux/debian \
-           $(lsb_release -cs) \
-           stable"
+        sudo apt-get -y install ca-certificates curl gnupg2 curl lsb-release software-properties-common apt-transport-https apt-utils
+        curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+        echo \
+            "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+            $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
         sudo apt-get update
         sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-        sudo groupadd docker
+        getent group docker >/dev/null || sudo groupadd docker
         sudo usermod -aG docker vagrant
-        sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
 SCRIPT
     config.vm.provision "install-docker", type: "shell", privileged: false, reset: true, inline: $script
